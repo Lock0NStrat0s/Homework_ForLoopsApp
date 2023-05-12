19d09cc436db2dcbb910d23236e68bfb45bdb911
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-12 00:35:37 -0600

    Add project files.

---
+++ Homework_ForLoops/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Ask user for a comma-separated list of first names.
+// Split string into string array.
+// Loop through array and print "Hello <name>" to the console.
+
+//ask user for names
+Console.Write("Enter list of comma-separated names: ");
+string? data = Console.ReadLine();
+
+//place each name into array
+string[] names = data.Split(',');
+
+//loop and display names
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine($"Hello {names[i]}");
+}
